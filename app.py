5709692aafe14f60fc0b02fc3e74e3e61f1bf077
Hoàng Vũ <hoangvu03081@gmail.com>
2021-12-17 18:24:58 +0700

    Update logic

--- app.py
+++ app.py
@@ -13,7 +13,8 @@ from Astar.utils import generate_cnf_clauses
 class StateType:
     INITIAL = 1 # App has just started, no file selected
     LOADING = 2 # A file is selected, the file's content is being processed
-    LOADED = 3 # The state is ready
+    LOADED = 3 # The algorithm has finished processing
+    VISUALIZE_EXPANDED_LIST = 4 # The state which program will automatically visualize expanded list step by step
 class App(QMainWindow):
     def __init__(self):
         super(App, self).__init__()
@@ -63,6 +64,7 @@ class App(QMainWindow):
         self.visualizePathButton.setStyleSheet(style.visualizePathButtonStyle)
         self.visualizePathButton.setText("VISUALIZE PATH")
         self.visualizePathButton.setCursor(QCursor(Qt.PointingHandCursor))
+        self.visualizePathButton.clicked.connect(self.chooseVisualPath)
         self.visualizePathButton.setEnabled(False)
 
         self.visualizeExpandedListButton = QtWidgets.QPushButton(self)
@@ -70,6 +72,7 @@ class App(QMainWindow):
         self.visualizeExpandedListButton.setStyleSheet(style.visualizeExpandedListButtonStyle)
         self.visualizeExpandedListButton.setCursor(QCursor(Qt.PointingHandCursor))
         self.visualizeExpandedListButton.setText("VISUALIZE EXPANDED LIST")
+        self.visualizeExpandedListButton.clicked.connect(self.chooseExpandedList)
         self.visualizeExpandedListButton.setEnabled(False)
 
         self.saveCNFButton = QtWidgets.QPushButton(self)
@@ -79,6 +82,9 @@ class App(QMainWindow):
         self.saveCNFButton.setText("SAVE CNF TO TEXT FILE")
         self.saveCNFButton.clicked.connect(self.saveCNF)
         self.saveCNFButton.setEnabled(False)
+
+        self.timer=QTimer(self)
+        self.timer.timeout.connect(self.updateExpandedList)
 
     
     def initBoard(self):
@@ -123,7 +129,13 @@ class App(QMainWindow):
             inputData = Utils.load(fileName)
             queenSolver = QueenSolver(initial_state=inputData[0], remain=8-inputData[1])
             path, expanded_list, time = queenSolver.solve()
-            return self.updateState({"type": StateType.LOADED, "path": path, "expanded_list": expanded_list, "time": int(time*1000), "index": 0, "maxIndex":len(path)-1 })
+            return self.updateState(
+                {"type": StateType.LOADED, 
+                "path": path, 
+                "expanded_list": expanded_list, 
+                "time": int(time*1000), 
+                "index": 0, 
+                "maxIndex":len(path)-1 })
         return self.updateState(self.state)
 
     def initQueenList(self):
@@ -151,6 +163,15 @@ class App(QMainWindow):
         self.updateVisualPathButton()
         self.updateVisualizeExpandedListButton()
         self.updateSaveCNFButton()
+        self.updateTimer()
+
+    def updateTimer(self):
+        stateType = self.state.get("type")
+        if stateType == StateType.VISUALIZE_EXPANDED_LIST:
+            if self.state.get("index") >= len(self.state.get("expanded_list"))-1:
+                self.timer.stop()
+        else: 
+            self.timer.stop()
 
     def updateVisualPathButton(self):
         stateType = self.state.get("type")
@@ -186,6 +207,8 @@ class App(QMainWindow):
             self.leftButton.setEnabled(False)
         if stateType == StateType.LOADED:
             self.leftButton.setEnabled(True)
+        if stateType == StateType.VISUALIZE_EXPANDED_LIST:
+            self.leftButton.setEnabled(False)
                 
 
     def updateRightButton(self):
@@ -194,6 +217,8 @@ class App(QMainWindow):
             self.rightButton.setEnabled(False)
         if stateType == StateType.LOADED:
             self.rightButton.setEnabled(True)
+        if stateType == StateType.VISUALIZE_EXPANDED_LIST:
+            self.rightButton.setEnabled(False)
                 
     def decreaseStateIndex(self):
         if (self.state.get("index") > 0):
@@ -214,9 +239,17 @@ class App(QMainWindow):
             
     def updateBoard(self):
         stateType = self.state.get("type")
+
         if stateType == StateType.LOADED:
             self.clearQueen()
             currentBoard = self.state.get("path")[self.state.get("index")]
+            queenIndexList = [i for i, ltr in enumerate(currentBoard) if ltr == "1"]
+            for queenIndex in queenIndexList:
+                self.spawnQueen(queenIndex)
+
+        elif stateType == StateType.VISUALIZE_EXPANDED_LIST:
+            self.clearQueen()
+            currentBoard = self.state.get("expanded_list")[self.state.get("index")]
             queenIndexList = [i for i, ltr in enumerate(currentBoard) if ltr == "1"]
             for queenIndex in queenIndexList:
                 self.spawnQueen(queenIndex)
@@ -263,6 +296,31 @@ class App(QMainWindow):
                 cnf = generate_cnf_clauses(self.state.get("path")[self.state.get("index")])
                 with open(fileName, "w") as fileStream:
                     fileStream.writelines(cnf)
+            elif stateType == StateType.VISUALIZE_EXPANDED_LIST:
+                cnf = generate_cnf_clauses(self.state.get("path")[self.state.get("maxIndex")])
+                with open(fileName, "w") as fileStream:
+                    fileStream.writelines(cnf)
+
+    def chooseVisualPath(self):
+        stateType = self.state.get("type")
+        if stateType == StateType.LOADED or stateType == StateType.VISUALIZE_EXPANDED_LIST:
+            if self.timer.isActive():
+                self.timer.stop()
+            self.updateState({"type": StateType.LOADED, "index": 0})
+    
+    def chooseExpandedList(self):
+        if self.timer.isActive():
+            self.timer.stop()
+        self.updateState({"type": StateType.VISUALIZE_EXPANDED_LIST, "index": 0})
+        self.timer.start(500)
+
+    def updateExpandedList(self):
+        stateType = self.state.get("type")
+        print(self.state.get("index"))
+        if stateType == StateType.VISUALIZE_EXPANDED_LIST and self.state.get("index") < len(self.state.get("expanded_list"))-1:
+            self.updateState({"type": StateType.VISUALIZE_EXPANDED_LIST, "index": self.state.get("index") + 1})
+
+
 
 def run():
     app = QApplication(sys.argv)
